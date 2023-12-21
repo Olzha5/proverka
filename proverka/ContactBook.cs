748a91c4e1c80640d7adb6feb25c88698d77373f
Olzhas <olzhasabukhan2003@gmail.com>
2023-12-21 14:17:08 +0600

    Добавьте файлы проекта.

---
+++ proverka/ContactBook.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Name: {FirstName} {LastName}, Phone: {PhoneNumber}, Email: {Email}";
+    }
+}
+
+public class ContactBook
+{
+    private List<Contact> contacts = new List<Contact>();
+    private int nextId = 1;
+    private readonly string FilePath = "contacts.json";
+
+    public ContactBook()
+    {
+        LoadContactsFromFile();
+    }
+
+    public void AddContact(string firstName, string lastName, string phoneNumber, string email)
+    {
+        var newContact = new Contact
+        {
+            Id = nextId++, // Auto-increment the ID
+            FirstName = firstName,
+            LastName = lastName,
+            PhoneNumber = phoneNumber,
+            Email = email
+        };
+
+        contacts.Add(newContact);
+        SaveContactsToFile(); // Save changes immediately
+        Console.WriteLine("Contact added successfully:\n{0}", newContact);
+    }
+
+    public void SaveContactsToFile()
+    {
+        string jsonString = JsonSerializer.Serialize(contacts);
+        File.WriteAllText(FilePath, jsonString);
+    }
+
+    public void LoadContactsFromFile()
+    {
+        if (File.Exists(FilePath))
+        {
+            string jsonString = File.ReadAllText(FilePath);
+            var loadedContacts = JsonSerializer.Deserialize<List<Contact>>(jsonString);
+            if (loadedContacts != null)
+            {
+                contacts = loadedContacts;
+                nextId = contacts.Any() ? contacts.Max(c => c.Id) + 1 : 1;
+            }
+        }
+    }
+
+
+// EditContact изменяет существующий контакт, найденный по ID.
+        public void EditContact(int id, string firstName, string lastName, string phoneNumber, string email)
+        {
+            var contact = contacts.FirstOrDefault(c => c.Id == id);
+            if (contact != null)
+            {
+                contact.FirstName = firstName;
+                contact.LastName = lastName;
+                contact.PhoneNumber = phoneNumber;
+                contact.Email = email;
+                Console.WriteLine("Contact updated successfully:\n{0}", contact);
+            }
+            else
+            {
+                Console.WriteLine($"Contact with ID {id} not found.");
+            }
+        }
+
+    // DeleteContact удаляет контакт по указанному ID.
+    public void DeleteContact(int id)
+    {
+        var contact = contacts.FirstOrDefault(c => c.Id == id);
+        if (contact != null)
+        {
+            contacts.Remove(contact);
+            SaveContactsToFile(); // Если вы сохраняете изменения после каждой операции
+            Console.WriteLine($"Contact with ID {id} deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine($"Contact with ID {id} not found.");
+        }
+    }
+
+    // ListAllContacts выводит все контакты в консоль.
+    public void ListAllContacts()
+        {
+            if (contacts.Any())
+            {
+                Console.WriteLine("Contacts:");
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine(contact);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No contacts found.");
+            }
+        }
+
+        internal void AddContact(object firstName, object lastName, object phoneNumber, object email)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddContact(string firstName, object lastName, object phoneNumber, object email)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddContact(string firstName, object lastName, string phoneNumber, object email)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddContact(string firstName, string lastName, string phoneNumber, object email)
+        {
+            throw new NotImplementedException();
+        }
+}
---
+++ proverka/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Program
+{
+    // Главный класс для управления книгой контактов в консольном приложении.
+    static ContactBook contactBook = new ContactBook();
+    private static string firstName;
+    private static string phoneNumber;
+    private static string lastName;
+    private static string email;
+
+    // Главная точка входа для приложения.
+    static void Main(string[] args)
+    {
+        // Простейшая система входа для демонстрационных целей
+        Console.Write("Enter login: ");
+        string login = Console.ReadLine();
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        if (login == "admin" && password == "admin") // Проверка учетных данных
+        {
+            while (true)
+            {
+                Console.WriteLine("\nChoose an option:");
+                Console.WriteLine("1: Add Contact");
+                Console.WriteLine("2: Search Contacts");
+                Console.WriteLine("3: Edit Contact");
+                Console.WriteLine("4: Delete Contact");
+                Console.WriteLine("5: List All Contacts");
+                Console.WriteLine("6: Exit");
+                Console.Write("Enter option: ");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        AddContact();
+                        break;
+                    case "2":
+                        SearchContacts();
+                        break;
+                    case "3":
+                        EditContact();
+                        break;
+                    case "4":
+                        DeleteContact();
+                        break;
+                    case "5":
+                        ListAllContacts();
+                        break;
+                    case "6":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid credentials.");
+        }
+    }
+
+    static void AddContact()
+    {
+        Console.Write("Enter first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter phone number: ");
+        string phoneNumber = Console.ReadLine();
+        Console.Write("Enter email: ");
+        string email = Console.ReadLine();
+
+        contactBook.AddContact(firstName, lastName, phoneNumber, email);
+    }
+    // Поиск контактов на основе запроса пользователя.
+    static void SearchContacts()
+    {
+        Console.Write("Enter search query: ");
+        // ...
+        // Выводим все найденные контакты.
+        // ...
+    }
+
+    // Редактирует существующий контакт, идентифицированный по ID.
+    static void EditContact()
+    {
+        Console.Write("Enter the ID of the contact to edit: ");
+        // ...
+        // Обновляем информацию контакта и выводим результат.
+        // ...
+    }
+
+    // Удаляет контакт по ID, предоставленному пользователем.
+    static void DeleteContact()
+    {
+        Console.Write("Enter the ID of the contact to delete: ");
+        if (int.TryParse(Console.ReadLine(), out int id))
+        {
+            contactBook.DeleteContact(id);
+            Console.WriteLine($"Contact with ID {id} deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid ID format.");
+        }
+    }
+
+    // Отображает список всех контактов в книге.
+    static void ListAllContacts()
+    {
+        contactBook.ListAllContacts();
+    }
+}
